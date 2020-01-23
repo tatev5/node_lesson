7ebff902d1aa171db96e224fccdce4cdea69863a
tatev <tatevs85@gmail.com>
2020-01-24 00:51:03 +0400

    'login-validation'

--- routes/index.js
+++ routes/index.js
@@ -27,15 +27,23 @@ router.post('/login',[
   check('name').isAlpha(),
 	check('name').isLength({min:4, max:20}),
   check('email', 'Սխալ մուտքագրված էլ-հասցե').isEmail(),
-	check('password', 'Գաղտնաբեռը պետք է լինի ոչ պակաս քան 4 տառ').isLength({min:4})
+	check('password', 'Գաղտնաբեռը պետք է լինի ոչ պակաս քան 5 տառ').isLength({min:4})
 	],
 	(req,res)=>{
 	const errors = validationResult(req);
 	
 	if (!errors.isEmpty()) {
+		console.log(errors);
+		
 		return res.status(422).json({ errors: errors.array() });
 	}
-	let e_mail = req.body.email;
+	let name=req.body.name;
+	let email = req.body.email;
+	let password=req.body.password
+	console.log('name is  '+ name);
+	console.log('mail is  ' +email);
+	console.log('password is ' +password);
+	
 	
 		
     res.render('sign')
